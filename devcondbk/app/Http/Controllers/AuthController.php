384f077796c90baa6edbbe80616096bd12cc199c
Natan Trennepohl <natan.gabriel@unoesc.edu.br>
2021-06-24 11:28:06 -0300

    criação de rotas e controller de autenticação

--- devcondbk/app/Http/Controllers/AuthController.php
+++ devcondbk/app/Http/Controllers/AuthController.php
@@ -3,8 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User; 
 
 class AuthController extends Controller
 {
-    //
+    // função que retorna um json mostrando que o usuário não é autorizado
+    public function unauthorized() {
+        return  response()->json([
+            'error' => 'Não Autorizado'
+        ], 401);
+    }
+
+    //função de registro recebendo os dados enviados através do request
+    public function register(Request $request){
+        $array = ['error' => ''];
+
+        //validando as informações fornecidas pelo usuário
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'cpf' => 'required|digits:11|unique:users,cpf',
+            'password' => 'required',
+            'password_confirm' => 'required|same:password'
+        ]);
+
+        if(!$validator->fails()) {
+            //pegando informações fornecidas pelo usuário e salvando
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            //criptografando a senha no padrão do php
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            //criando o novo usuário no banco e salvando.
+            $newUser = new User();
+
+            $newUser->name = $name;
+            $newUser->email = $email;
+            $newUser->cpf = $cpf;
+            $newUser->password = $hash;
+            $newUser->save();
+
+        }else {
+            //código de erro caso o validador falhe
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
+
 }
--- devcondbk/routes/api.php
+++ devcondbk/routes/api.php
@@ -17,3 +17,67 @@ use App\Http\Controllers\WarningController;
 Route::get('/ping', function(){
         return ['pong'=>true];
 });
+
+//rota não autorizado, ou sem login
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login'); 
+
+
+//rota para fazer login e registro.
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/register', [AuthController::class, 'register']);
+
+
+// grupo de rotas em que o usuário precisa estar logado para acessar
+Route::middleware('auth:api')->group(function(){
+        
+        Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+        //Mural de Avisos       
+
+        //rota para pegar todos os likes
+        Route::get('/walls', [WallController::class, 'getAll']);
+        //rota para dar e tirar likes
+        Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+        //documentos
+        Route::get('/dosc', [DocController::class, 'getAll']);
+
+        //Livro de Ocorrências
+        Route::get('/warnings', [WarningController::class, 'getMyWarnings']);
+        Route::post('/warning', [WarningController::class, 'setWarning']);
+        Route::post('warning/file', [WarningController::class, 'addWarningFile']);
+
+        //Boletos
+        Route::get('/billets',[BilletController::class, 'getAll']);
+
+        //Achados e Perdidos
+        Route::get('/foundandlost', [FoundAndLostController::class, 'getAll']);
+        Route::post('/foundandlost', [FoundAndLostController::class, 'insert']);
+        Route::put('foundandlost/{id}', [FoundAndLostController::class, 'update']);
+
+        //Unidade
+        Route::get('/unit/{id}', [UnitController::class, 'getInfo']);
+        Route::post('/unit/{id}/addperson', [UnitController::class, 'addPerson']);
+        Route::post('/unit/{id}/addvehicle', [UnitController::class, 'addVehicle']);
+        Route::post('/unit/{id}/addpet', [UnitController::class, 'addPet']);
+        Route::post('/unit/{id}/removeperson', [UnitController::class, 'removePerson']);
+        Route::post('/unit/{id}/removevehicle', [UnitController::class, 'removeVehicle']);
+        Route::post('/unit/{id}/removepet', [UnitController::class, 'removePet']);
+
+        //reservas
+        Route::get('/reservation', [ReservationController::class, 'getReservations']);
+        Route::get('/myreservations', [ReservationController::class, 'getMyReservations']);
+
+        Route::get('/reservation/{id}/disableddates', [ReservationController::class, 'getDisabledDates']);
+        Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
+
+        Route::post('/deletemyreservation/{id}', [ReservationController::class, 'delMyReservation']);
+        Route::post('/reservation/{id}', [ReservationController::class, 'setReservation']);
+        
+});
+
+
+
+
+
