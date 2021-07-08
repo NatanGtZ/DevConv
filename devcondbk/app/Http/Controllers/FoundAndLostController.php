ba6d5d5732b04af34a36614a25407bd3f57a3b02
Natan Trennepohl <natan.gabriel@unoesc.edu.br>
2021-07-08 16:00:22 -0300

    atts

--- devcondbk/app/Http/Controllers/FoundAndLostController.php
+++ devcondbk/app/Http/Controllers/FoundAndLostController.php
@@ -4,20 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use app\Models\FoundAndLost;
 
 class FoundAndLostController extends Controller
 {
     public function getAll() {
         $array = ['error' => ''];
 
-        
-        $lost = FoundAndLost::where('status', 'LOST')
+        $lost = foundAndLost::where('status', 'LOST')
         ->orderBy('datecreated', 'DESC')
         ->orderBy('id', 'DESC')
-        -get();
+        ->get();
 
         $array['lost'] = $lost; 
+     
 
 
 
